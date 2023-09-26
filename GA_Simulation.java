8a59c65b988d848b601d04e529ba06a7d6c3e22b
mneedham01 <mneedham@smith.edu>
2023-09-26 17:54:23 -0400

    main method, describeGeneration, evolve

--- GA_Simulation.java
+++ GA_Simulation.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class GA_Simulation {
     // number of individuals in a population
@@ -7,7 +8,7 @@ public class GA_Simulation {
     // number of winners
     int k;
     // number of evolution rounds to run
-    int r;
+    int r = 100;
 
     /*
      * Constructor stores experimental parameters in field
@@ -42,5 +43,62 @@ public class GA_Simulation {
       pop.sort(ranker);
     }
 
+    /*
+     * Selects winners and then randomly selects two parents
+     * for each member of the new generation
+     */
+    public ArrayList<Individual> evolve(ArrayList<Individual> pop) {
+        ArrayList<Individual> newGen = new ArrayList<Individual>();
+        // selects winners of first population
+        ArrayList<Individual> winners = new ArrayList<Individual>();
+        for (int i = 0; i < this.k; i++) {
+            winners.add(pop.get(i));
+        }
+        // loop for each new individual
+        for (int i = 0; i < this.n; i++) {
+            // randomly selects two parents
+            int num1 = ThreadLocalRandom.current().nextInt(0, this.k);
+            Individual p1 = winners.get(num1);
+            int num2 = ThreadLocalRandom.current().nextInt(0, this.k);
+            // ensure individual cannot breed with themselves
+            while (num1 == num2){
+                num2 = ThreadLocalRandom.current().nextInt(0, this.k);
+            }
+            Individual p2 = winners.get(num2);
+            // create new individual and add to ArrayList
+            Individual offspring = new Individual(p1.chromosome, p2.chromosome);
+            newGen.add(offspring);
+        }
+        return newGen;
+    }
+
+    /*
+     * Prints statistics about the current generation
+     */
+    public void describeGeneration(ArrayList<Individual> pop) {
+        Individual top = pop.get(0);
+        Individual bottom = pop.get(0);
+        for (int i = 1; i < pop.size(); i++) {
+            if (pop.get(i).getFitness() > top.getFitness()) {
+                top = pop.get(i);
+            }
+            if (pop.get(i).getFitness() < bottom.getFitness()) {
+                bottom = pop.get(i);
+            }
+        }
+        System.out.println("Highest fitness score: "+ top.getFitness());
+        System.out.println("Highest fitness chromosome: "+ top.toString());
+        System.out.println("Lowest fitness score: "+ bottom.getFitness());
+    }
+
+    public static void main(String[] args) {
+        GA_Simulation experiment = new GA_Simulation();
+        ArrayList<Individual> generation = experiment.init();
+        for (int i = 0; i < 100; i++) {
+            System.out.println("\nGeneration: "+i);
+            generation = experiment.evolve(generation);
+            experiment.describeGeneration(generation);
+        }
+    }
 
 }
